c3ed5ec7d816844ea9652dd273beb658d9cb9eb3
parsgit <nvioir@gmail.com>
2025-04-25 22:07:02 +0330

    improve dashboard and header title in admin panel

--- assets/PostOldView-BjLoXljK.js
+++
@@ -1,1 +0,0 @@
-import{l as t,c as o,o as s}from"./index-DNHKJcl9.js";const l={__name:"PostOldView",setup(a){const e=t();return window.location.href=`/post/${e.params.title}`,(r,c)=>(s(),o("div",null," ... "))}};export{l as default};
