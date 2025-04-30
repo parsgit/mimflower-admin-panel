6517cbbe9acccc968ad415a999b0373e44798025
parsgit <nvioir@gmail.com>
2025-04-30 21:59:37 +0330

    update order

---
+++ assets/PostOldView-BJYcUBj6.js
@@ -0,0 +1,1 @@
+import{l as t,c as o,o as s}from"./index-P-9bCYUJ.js";const l={__name:"PostOldView",setup(a){const e=t();return window.location.href=`/post/${e.params.title}`,(r,c)=>(s(),o("div",null," ... "))}};export{l as default};
