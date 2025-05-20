f1554d4087d11686e1ee5914149c485a0bad0099
parsgit <nvioir@gmail.com>
2025-05-20 19:50:17 +0330

    update

--- assets/PostOldView-Pn3ip9AM.js
+++
@@ -1,1 +0,0 @@
-import{m as t,c as o,o as s}from"./index-B_dLCW4e.js";const l={__name:"PostOldView",setup(a){const e=t();return window.location.href=`/post/${e.params.title}`,(r,c)=>(s(),o("div",null," ... "))}};export{l as default};
